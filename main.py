6854eeb2afa0c45f1335a5fb7e81f32c93886b1d
Tomasz Tarabasz <tomektarabasz@gmail.com>
2020-02-16 17:43:35 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import torch
+import torchvision
+
+import atexit
+import bisect
+import multiprocessing as mp
+from collections import deque
+import cv2
+import torch
+
+from detectron2.data import MetadataCatalog
+from detectron2.engine.defaults import DefaultPredictor
+from detectron2.utils.video_visualizer import VideoVisualizer
+from detectron2.utils.visualizer import ColorMode, Visualizer
+
+print("dsfsdf")
